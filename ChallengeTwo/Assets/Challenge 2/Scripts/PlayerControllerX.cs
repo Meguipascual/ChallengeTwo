fdf7ceb41e9fbd84ec1fd43401bfb78f117bb5d6
meguipascual <meguipascual@gmail.com>
2021-09-16 07:13:43 +0200

    Work done, the extra goals are not a success even with the hint that they purpose this don't work so, whatever

--- ChallengeTwo/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ ChallengeTwo/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -9,6 +9,9 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
+        
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
